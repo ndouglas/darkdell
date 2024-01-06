dbadf4aaa17aaa395c07059fff82dba78a726a88
Nathan Douglas <github@darkdell.net>
2024-01-05 22:42:05 -0500

    Index.

--- content/index.md
+++ content/index.md
@@ -4,3 +4,17 @@ date: 2023-02-18T00:06:13-05:00
 draft: false
 type: 'index'
 ---
+<!-- more -->
+Hello world!
+I'm apparently dumb enough to write my own SSG, so... watch this space. And don't expect much. :slightly_smiling_face:
+
+(I don't have code in place to do this automatically yet. Forgive me.)
+
+# Special Pages
+- [About Me](./about.html)
+
+# Posts
+
+- [Have You Seen Me Lately?](./posts/2024-01-03--have-you-seen-me-lately.html)
+- [What is Valuable?](./posts/2023-12-15--what-is-valuable.html)
+- [Dark Ecology](./posts/2023-12-14--dark-ecology.html)
--- src/renderer/mod.rs
+++ src/renderer/mod.rs
@@ -1,8 +1,9 @@
 use anyhow::Error as AnyError;
-use comrak::nodes::{AstNode, NodeValue};
-use comrak::{format_html, parse_document, Arena, Options};
+use comrak::{markdown_to_html, Options};
+use gray_matter::engine::YAML;
+use gray_matter::Matter;
+use log::trace;
 use maud::{html, Markup, PreEscaped, DOCTYPE};
-use std::mem::take;
 
 use crate::front_matter::FrontMatter;
 use crate::settings::Settings;
@@ -18,41 +19,61 @@ pub struct Renderer {
 impl Renderer {
   /// Create a new renderer.
   pub fn new(settings: Settings) -> Self {
-    Self {
-      settings,
-      options: Options::default(),
-    }
+    let mut options = Options::default();
+    options.extension.table = true;
+    options.extension.strikethrough = true;
+    options.extension.tasklist = true;
+    options.extension.footnotes = true;
+    options.extension.autolink = true;
+    options.extension.description_lists = true;
+    options.extension.superscript = true;
+    options.extension.header_ids = Some("section-".to_string());
+    options.extension.shortcodes = true;
+    options.render.hardbreaks = true;
+    // options.render.unsafe_ = true;
+    Self { settings, options }
+  }
+
+  /// Get only the front matter for a content file.
+  pub fn get_front_matter(&self, content_file: &str) -> Result<FrontMatter, AnyError> {
+    let mut matter = Matter::<YAML>::new();
+    matter.excerpt_delimiter = self.settings.excerpt_delimiter.clone();
+    let parsed_content = matter.parse(content_file);
+    trace!("Parsed content: {:#?}", parsed_content);
+    let front_matter: FrontMatter = parsed_content.data.unwrap().deserialize().unwrap();
+    Ok(front_matter)
   }
 
   /// Render some markdown.
   pub fn render(&self, front_matter: &FrontMatter, content: &str) -> Result<String, AnyError> {
-    let arena = Arena::new();
-    let root = parse_document(&arena, content, &self.options);
-    iter_nodes(root, &|node| {
-      if let &mut NodeValue::Text(ref mut text) = &mut node.data.borrow_mut().value {
-        let orig = take(text);
-        *text = orig.replace("my", "your");
-      }
-    });
-    let mut html = vec![];
-    format_html(root, &self.options, &mut html)?;
-    let content = String::from_utf8(html)?;
-    let result = self.render_html(front_matter, &content);
-    Ok(result.into_string())
+    let content_html = markdown_to_html(content, &self.options);
+    let rendered_html = self.render_html(front_matter, &content_html).into_string();
+    Ok(rendered_html)
   }
 
   /// Render the HTML.
   pub fn render_html(&self, front_matter: &FrontMatter, content: &str) -> Markup {
+    let description = front_matter
+      .description
+      .clone()
+      .unwrap_or(self.settings.description.clone().unwrap());
+    let author = front_matter
+      .author
+      .clone()
+      .unwrap_or(self.settings.author.clone().unwrap());
+    let title = front_matter.title.clone().unwrap_or("Untitled".to_string());
     html! {
       (DOCTYPE)
       html lang="en" {
         head {
           meta charset="utf-8";
           meta name="viewport" content="width=device-width, initial-scale=1";
-          meta name="description" content=(front_matter.description.clone().unwrap_or(self.settings.description.clone().unwrap()));
-          meta name="author" content=(front_matter.author.clone().unwrap_or(self.settings.author.clone().unwrap()));
+          meta name="description" content=(description);
+          meta name="author" content=(author);
           meta name="generator" content="Darkdell";
-          //title (title)
+          title {
+            (title)
+          }
           link rel="stylesheet" href="/css/style.css";
         }
         body {
@@ -88,16 +109,5 @@ impl Renderer {
         }
       }
     }
-  }
-}
-
-/// Iterate over the nodes.
-fn iter_nodes<'a, F>(node: &'a AstNode<'a>, f: &F)
-where
-  F: Fn(&'a AstNode<'a>),
-{
-  f(node);
-  for c in node.children() {
-    iter_nodes(c, f);
   }
 }
